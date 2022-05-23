c1afee974f509e718e9184493205583611cfc3b8
capushynoo <105780978+capushynoo@users.noreply.github.com>
2022-05-23 17:31:41 +0600

    Add files via upload

---
+++ books/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("signup/", views.Usersignup, name="Usersignup"),
+    path("user_login/", views.User_login, name="User_login"),
+    path("logout/", views.Logout, name="Logout"),
+    path("for_users/", views.Users, name="Users"),
+    path("checkout/", views.checkout, name="checkout"),
+    path("bookcrossing/", views.book_crossing, name="bookcrossing"),
+    path("see_bookcrossing/", views.see_requested_books, name="see_bookcrossing"),
+    path("request_books/", views.request_books, name="request_books"),
+    path("see_requested_books/", views.see_requested_books, name="see_requested_books"),
+
+]
